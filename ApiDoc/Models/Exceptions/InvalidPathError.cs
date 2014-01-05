6e594dae505ea69924f7c3a7843c0b3b0b803a8a
Immanuel Hayden <immanuel.hayden@gmail.com>
2014-01-05 21:41:26 +0100

    add tinymce

---
+++ ApiDoc/Models/Exceptions/InvalidPathError.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ApiDoc.Models.Exceptions
+{
+    public class InvalidPathError : Exception
+    {
+        public InvalidPathError() { }
+        public InvalidPathError(string message) : base(message){}
+    }
+}
---
+++ ApiDoc/Models/Exceptions/InvalidPathWarning.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ApiDoc.Models.Exceptions
+{
+    public class InvalidPathWarning : Exception
+    {
+        public InvalidPathWarning() { }
+        public InvalidPathWarning(string message) : base(message) { }
+    }
+}
